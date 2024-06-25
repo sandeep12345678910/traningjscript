bb9de59962d81f511668bed7d1c0ddd29c5c0c6e
sandeep12345678910 <sandeeppok202@gmail.com>
2024-06-25 09:27:27 +0545

    variable

---
+++ script.js
@@ -0,0 +1,31 @@
+//variables
+
+//var
+//var x=90;
+//console.log(x);
+
+//var x= 180(x);
+//console.log(x);
+
+//let
+let y=90;
+console.log(y);
+
+//cannot be redeclared
+//let y=10;
+
+//can be updated
+// y=20;
+// console.log(y);
+
+//const
+const z=10;
+console.log(z);
+
+//cannot be redeclared
+//cannot be updated
+z=9;
+
+const output = "hello world!";
+
+console.log(typeof output);
